01dc07fc6e008543a1aefdb31129b57e68bb0857
jerome4914 <jeremyballew@hotmail.com>
2021-07-08 03:15:43 -0600

    changes from shell to template

--- main/models.py
+++ main/models.py
@@ -1,14 +1,35 @@
 from django.db import models
 
+# class AuthorManager(models.Manager):
+#     def author_validator(self, postData):
+#         errors= {}
+#         if len(postData['first_name']) < 2:
+#             errors['first_name']= "First name must be at least 2 characters long!"
+#         if len(postData['last_name']) < 2:
+#             errors['last_name']= "Last name must be at least 2 characters long!"
+#         if len(postData['notes']) < 5:
+#             errors['notes']= "Notes must be at least 5 characters long!"
+#         return errors
 # Create your models here.
+# class BookManager(models.Manager):
+#     def book_validator(self, postData):
+#         errors= {}
+#         if len(postData['title']) < 2:
+#             errors['title']= "Title must be at least 2 characters long!"
+#         if len(postData['description']) < 2:
+#             errors['description']= "Description must be at least 2 characters long!"
+#         return errors
+
+
+    
 class Book(models.Model):
     title = models.CharField(max_length=255)
     description = models.TextField()
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
 
-    def __repr__(self):
-        return "Title: {}".format(self.title)
+    # def __repr__(self):
+    #     return "Title: {}".format(self.title)
 
 class Author(models.Model):
     first_name = models.CharField(max_length=45)
@@ -18,5 +39,6 @@ class Author(models.Model):
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
 
-    def __repr__(self):
-        return "Name: {}".format(self.first_name, self.last_name)
+    # This was for use in the shell so that I could see the Auther and the book names instead of id!!
+    # def __repr__(self):
+    #     return "Name: {}".format(self.first_name, self.last_name)
--- main/urls.py
+++ main/urls.py
@@ -2,5 +2,14 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.index),
+    path('books', views.books),
+    path('books/create', views.create_book),
+    path('books/<int:id>', views.one_book),
+    path('books/<int:id>/delete', views.delete_book),
+    path('books/add_author', views.add_author),
+    path('authors', views.authors),
+    path('authors/create', views.create_author),
+    path('authors/<int:id>', views.one_author),
+    path('authors/<int:id>/delete', views.delete_author),
+    path('authors/add_book', views.add_book)
 ]
